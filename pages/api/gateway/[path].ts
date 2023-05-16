786667e2a21da563a48334dd98fa57dd506d3ba9
zoubingwu <zoubingwu@gmail.com>
2023-05-16 20:31:16 +0800

    Rename env variables

--- pages/api/gateway/[path].ts
+++ pages/api/gateway/[path].ts
@@ -1,6 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { fetcher } from "~/lib/fetch";
+import DigestFetch from "with-digest-fetch";
+
+const client = new DigestFetch(
+  process.env.TIDBCLOUD_DATA_SERVICE_PUBLIC_KEY!,
+  process.env.TIDBCLOUD_DATA_SERVICE_PRIVATE_KEY!
+);
+
+const fetcher = async (key: string, body?: Record<string, any>) => {
+  const url = `${process.env.TIDBCLOUD_DATA_SERVICE_HOST}/${key}`;
+  const res = await client.fetch(url, {
+    body: body ? JSON.stringify(body) : body,
+  });
+  return await res.json();
+};
 
 export default async function handler(
   req: NextApiRequest,
